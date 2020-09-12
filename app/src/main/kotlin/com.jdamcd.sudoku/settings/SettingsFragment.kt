6865fdc2c08d7916e33131ce019cd217372dfff4
Jamie McDonald <jdamcd@gmail.com>
2020-09-12 15:24:55 +0100

    Improved system/dark theme controls

--- app/src/main/kotlin/com.jdamcd.sudoku/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com.jdamcd.sudoku/settings/SettingsFragment.kt
@@ -2,7 +2,6 @@ package com.jdamcd.sudoku.settings
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
-import android.os.Handler
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -13,18 +12,18 @@ import androidx.preference.Preference.OnPreferenceClickListener
 import androidx.preference.PreferenceFragmentCompat
 import com.jdamcd.sudoku.IntentFactory
 import com.jdamcd.sudoku.R
+import com.jdamcd.sudoku.settings.user.Settings
 import com.jdamcd.sudoku.util.AppInfo
 import com.jdamcd.sudoku.util.snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceClickListener, OnPreferenceChangeListener {
+class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceChangeListener {
 
     @Inject internal lateinit var info: AppInfo
     @Inject internal lateinit var intents: IntentFactory
-
-    private val handler = Handler()
+    @Inject internal lateinit var settings: Settings
 
     init {
         setHasOptionsMenu(true)
@@ -33,20 +32,18 @@ class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceClickListener, 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.settings)
         findPreference<Preference>(KEY_VERSION)?.summary = info.getVersion()
+        findPreference<Preference>(KEY_NIGHT_MODE)?.isVisible = !settings.isSystemTheme
         setListeners()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        handler.removeCallbacksAndMessages(null)
-    }
-
     private fun setListeners() {
-        findPreference<Preference>(KEY_HOW_TO_PLAY)?.onPreferenceClickListener = this
-        findPreference<Preference>(KEY_CHEATS)?.onPreferenceChangeListener = this
-        findPreference<Preference>(KEY_TIMER)?.onPreferenceChangeListener = this
-        findPreference<Preference>(KEY_SHOW_ERRORS)?.onPreferenceChangeListener = this
-        findPreference<Preference>(KEY_DARK_MODE)?.onPreferenceChangeListener = this
+        listOf(KEY_CHEATS, KEY_TIMER, KEY_SHOW_ERRORS, KEY_SYSTEM_THEME, KEY_NIGHT_MODE)
+            .forEach { findPreference<Preference>(it)?.onPreferenceChangeListener = this }
+
+        findPreference<Preference>(KEY_HOW_TO_PLAY)?.onPreferenceClickListener = OnPreferenceClickListener {
+            startActivity(intents.getHowToPlay())
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -81,22 +78,30 @@ class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceClickListener, 
         }
     }
 
-    override fun onPreferenceClick(preference: Preference): Boolean {
-        startActivity(intents.getHowToPlay())
-        return true
-    }
-
     override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
         when (preference.key) {
-            KEY_DARK_MODE -> toggleDarkMode(newValue as Boolean)
+            KEY_SYSTEM_THEME -> toggleSystemTheme(newValue as Boolean)
+            KEY_NIGHT_MODE -> toggleNightMode(newValue as Boolean)
         }
         return true
     }
 
-    private fun toggleDarkMode(enabled: Boolean) {
+    private fun toggleSystemTheme(enabled: Boolean) {
+        findPreference<Preference>(KEY_NIGHT_MODE)?.isVisible = !enabled
+        AppCompatDelegate.setDefaultNightMode(
+            when {
+                enabled -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                settings.isNightMode -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+        findPreference<Preference>(KEY_NIGHT_MODE)?.isVisible = !enabled
+    }
+
+    private fun toggleNightMode(enabled: Boolean) {
         AppCompatDelegate.setDefaultNightMode(
             if (enabled) AppCompatDelegate.MODE_NIGHT_YES
-            else AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            else AppCompatDelegate.MODE_NIGHT_NO
         )
     }
 
@@ -104,7 +109,8 @@ class SettingsFragment : PreferenceFragmentCompat(), OnPreferenceClickListener, 
         const val KEY_TIMER = "settings_timer"
         const val KEY_CHEATS = "settings_cheat"
         const val KEY_SHOW_ERRORS = "settings_show_errors"
-        const val KEY_DARK_MODE = "settings_dark_mode"
+        const val KEY_SYSTEM_THEME = "settings_system_theme"
+        const val KEY_NIGHT_MODE = "settings_night_mode"
         const val KEY_HOW_TO_PLAY = "settings_how_to_play"
         const val KEY_VERSION = "settings_version"
     }
